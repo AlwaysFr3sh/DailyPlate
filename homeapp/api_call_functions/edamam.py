e9a7ec697369c21d04249a6fb42c2044d1e22f32
jt01250 <jt01250@surrey.ac.uk>
2023-05-16 21:10:49 +0100

    added nutrition

--- homeapp/api_call_functions/edamam.py
+++ homeapp/api_call_functions/edamam.py
@@ -1,39 +1,40 @@
 import requests
 import json
 import logging
-import exceptions as e1
+
 logger = logging.getLogger("edamam")
 ##url -X GET --header "Accept: application/json" "https://api.edamam.com/api/nutrition-data?app_id=28946ca6&app_key=461321ba662873bae755da28b1c67945&nutrition-type=cooking&ingr=carrot"
-class edamam(object):
-    app_id= "7528744b"
-    app_key= "9d0aeab544b738d42bfa62125a86a94b"
-    
-    def get_nutritional_info(food_item):
-        base_url = 'https://api.edamam.com/api/nutrition-data'
-        params = {
-            'app_id': "28946ca6",
-           'app_key': "21f380c94135357b124aa67bf7d6db84",
-           'nutrition-type':"logging",
-           'ingr': food_item
-      }
 
-        response = requests.get(base_url, params=params)
+app_id= "7528744b"
+app_key= "9d0aeab544b738d42bfa62125a86a94b"
     
-        if response.status_code == 200:
-            data = response.json()
-            return data
-        else:
-            data = response.status_code
-            print(data)
-            
-            return None
-    result = get_nutritional_info("banana 500g")
+def get_nutritional_info(food_item):
+    base_url = 'https://api.edamam.com/api/nutrition-data'
+    params = {
+    'app_id': "28946ca6",
+    'app_key': "21f380c94135357b124aa67bf7d6db84",
+    'nutrition-type':"logging",
+    'ingr': food_item
+    }
 
-    if result:
-        print("Nutritional Information:")
-        print("Calories:", result['calories'])
-        print("Protein:", result['totalNutrients']['PROCNT']['quantity'], result['totalNutrients']['PROCNT']['unit'])
-        print("Carbohydrates:", result['totalNutrients']['CHOCDF']['quantity'], result['totalNutrients']['CHOCDF']['unit'])
-        print("Fat:", result['totalNutrients']['FAT']['quantity'], result['totalNutrients']['FAT']['unit'])
+    response = requests.get(base_url, params=params)
+    
+    if response.status_code == 200:
+        data = response.json()
+        
+        return data
     else:
-        print("Failed to retrieve nutritional information.")
+        
+        print(data)
+            
+        return None
+    # result = get_nutritional_info(ingredient)
+
+    # if result:
+    #     print("Nutritional Information:")
+    #     print("Calories:", result['calories'])
+    #     print("Protein:", result['totalNutrients']['PROCNT']['quantity'], result['totalNutrients']['PROCNT']['unit'])
+    #     print("Carbohydrates:", result['totalNutrients']['CHOCDF']['quantity'], result['totalNutrients']['CHOCDF']['unit'])
+    #     print("Fat:", result['totalNutrients']['FAT']['quantity'], result['totalNutrients']['FAT']['unit'])
+    # else:
+    #     print("Failed to retrieve nutritional information.")
